6d7faf3afc84d243fd74d277ca66adcf5b9896bb
pawelrus91 <18408679+pawelrus91@users.noreply.github.com>
2024-02-27 11:16:11 +0100

    create property cart component

---
+++ types/index.ts
@@ -0,0 +1,37 @@
+export type Property = {
+  _id: string;
+  owner: string;
+  name: string;
+  type: string;
+  description: string;
+  location: Location;
+  beds: number;
+  baths: number;
+  square_feet: number;
+  amenities: string[];
+  rates: Rates;
+  seller_info: SellerInfo;
+  images: string[];
+  is_featured: boolean;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type Location = {
+  street: string;
+  city: string;
+  state: string;
+  zipcode: string;
+};
+
+export type Rates = {
+  weekly: number;
+  monthly?: number;
+  nightly?: number;
+};
+
+export type SellerInfo = {
+  name: string;
+  email: string;
+  phone: string;
+};
