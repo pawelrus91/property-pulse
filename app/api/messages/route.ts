2a5db439735351b0aa7ec785ddfa8662a0a76043
pawelrus91 <18408679+pawelrus91@users.noreply.github.com>
2024-03-13 11:43:02 +0100

    create message POST route

---
+++ app/api/messages/route.ts
@@ -0,0 +1,68 @@
+import { NextRequest } from "next/server";
+import connectDB from "@/config/database";
+import Message from "@/models/Message";
+import { getSessionUser } from "@/utils/getSessionUser";
+
+export const dynamic = "force-dynamic";
+
+/**
+ * @route POST /api/messages
+ */
+export async function POST(request: NextRequest) {
+  try {
+    connectDB();
+
+    const { name, email, phone, body, property, recipient } =
+      await request.json();
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.user) {
+      return new Response(
+        JSON.stringify({ message: "You mast be logged in to send a message" }),
+        {
+          status: 401,
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+    }
+
+    const { user } = sessionUser;
+
+    // Can not send message to self
+    // @ts-expect-error
+    if (user.id === recipient) {
+      return new Response(
+        JSON.stringify({ message: "Can not send a message to yourself" }),
+        {
+          status: 400,
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+    }
+
+    const newMessage = new Message({
+      // @ts-ignore
+      sender: user.id,
+      recipient,
+      property,
+      email,
+      phone,
+      body,
+      name,
+    });
+
+    await newMessage.save();
+
+    return new Response(JSON.stringify({ message: " Message send" }), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.error(error);
+
+    return new Response("Something went wrong", {
+      status: 500,
+    });
+  }
+}
