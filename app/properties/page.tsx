b5874dd9845d1ecfd4b6c0bf7f9862d075692942
pawelrus91 <18408679+pawelrus91@users.noreply.github.com>
2024-03-15 08:49:48 +0100

    Refactoring Properties page

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,6 +1,6 @@
-import PropertyCard from "@/components/PropertyCard";
 import { fetchProperties } from "@/utils/requests";
 import PropertySearchForm from "@/components/PropertySearchForm";
+import Properties from "@/components/Properties";
 
 export default async function PropertiesPage() {
   const properties = await fetchProperties();
@@ -13,19 +13,7 @@ export default async function PropertiesPage() {
         </div>
       </section>
 
-      <section className="px-4 py-6">
-        <div className="container-xl lg:container m-auto px-4 py-6">
-          {properties.length === 0 ? (
-            <p>No search results found</p>
-          ) : (
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-              {properties.map((property) => (
-                <PropertyCard key={property._id} property={property} />
-              ))}
-            </div>
-          )}
-        </div>
-      </section>
+      <Properties />
     </>
   );
 }
---
+++ components/Properties.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { useState, useEffect } from "react";
+import { Property } from "@/types";
+import PropertyCard from "@/components/PropertyCard";
+import Spinner from "@/components/Spinner";
+
+export default function Properties() {
+  const [properties, setProperties] = useState<Property[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchProperties = async () => {
+      try {
+        const response = await fetch("/api/properties");
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+
+        const data = await response.json();
+        setProperties(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProperties();
+  }, []);
+
+  return (
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {properties.length === 0 ? (
+          <p>No search results found</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+}
